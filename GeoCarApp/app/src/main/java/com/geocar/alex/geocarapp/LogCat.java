12195661ec79796ff71ecc8bcec7c5ac4611892f
Matthew Kelly <badgerati42@gmail.com>
2015-10-24 15:51:52 +0100

    Login UI and logging logic

---
+++ GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/LogCat.java
@@ -0,0 +1,18 @@
+package com.geocar.alex.geocarapp;
+
+import android.util.Log;
+
+/**
+ * Created by Badgerati on 24/10/2015.
+ */
+public class LogCat
+{
+
+    public static void log(Object tag, Object message)
+    {
+        Log.v(
+            tag == null ? "NULL" : tag.getClass().getSimpleName(),
+            message == null ? "NULL" : message.toString());
+    }
+
+}
--- GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/LogIn.java
+++ GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/LogIn.java
@@ -1,19 +1,64 @@
 package com.geocar.alex.geocarapp;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LogIn extends AppCompatActivity
+public class LogIn extends AppCompatActivity implements View.OnClickListener
 {
+
+    private EditText mEmail = null;
+    private EditText mPassword = null;
+    private Button mSignIn = null;
+    private TextView mRegister = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mEmail = (EditText)findViewById(R.id.email_txt);
+        mPassword = (EditText)findViewById(R.id.password_txt);
+
+        mSignIn = (Button)findViewById(R.id.signin_btn);
+        mSignIn.setOnClickListener(this);
+
+        mRegister = (TextView)findViewById(R.id.register_lnk);
+        mRegister.setPaintFlags(mRegister.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        mRegister.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v)
+    {
+        if (mSignIn.equals(v))
+        {
+            doSignIn();
+        }
+        else if (mRegister.equals(v))
+        {
+            doRegister();
+        }
+    }
+
+    private void doSignIn()
+    {
+        //TODO: Send call to backend
+        LogCat.log(this, "HERE");
+    }
+
+    private void doRegister()
+    {
+        //TODO: Register activity
+        LogCat.log(this, "HERE2");
+    }
 }
