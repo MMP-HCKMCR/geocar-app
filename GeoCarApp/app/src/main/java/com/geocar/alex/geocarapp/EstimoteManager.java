daefc8ef0c6b5fb22f96aa98c546ed2dd35f1999
Matthew Kelly <badgerati42@gmail.com>
2015-10-24 22:58:11 +0100

    Added estimote notifications on success

--- GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/EstimoteManager.java
+++ GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/EstimoteManager.java
@@ -1,15 +1,18 @@
 package com.geocar.alex.geocarapp;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
+import com.geocar.alex.geocarapp.dto.RegisterTagResult;
 import com.geocar.alex.geocarapp.json.JsonDocument;
 import com.geocar.alex.geocarapp.web.IAsyncTask;
 import com.geocar.alex.geocarapp.web.WebRequest;
-import com.geocar.alex.geocarapp.web.WebResponse;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -20,7 +23,8 @@ public class EstimoteManager implements IAsyncTask.OnPostExecuteListener
     private BeaconManager mBeaconManager = null;
     private Region mRegion = null;
     private String mSessionId = "";
-    private HashMap<String, Long> mBeaconCache = null;
+    private Context mContext = null;
+    private HashMap<String, Calendar> mBeaconCache = null;
 
 
     public EstimoteManager(String sessionId)
@@ -31,6 +35,7 @@ public class EstimoteManager implements IAsyncTask.OnPostExecuteListener
 
     protected void startRanging(Context context)
     {
+        mContext = context;
         mBeaconManager = new BeaconManager(context);
         mRegion = new Region("Range Region", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
 
@@ -54,13 +59,14 @@ public class EstimoteManager implements IAsyncTask.OnPostExecuteListener
                     {
                         Beacon beacon = list.get(i);
                         String id = beacon.getMajor() + ":" + beacon.getMajor();
+                        Calendar calendar = Calendar.getInstance();
 
-                        if (mBeaconCache.containsKey(id))
+                        if (mBeaconCache.containsKey(id) && mBeaconCache.get(id).after(calendar))
                         {
                             continue;
                         }
 
-                        mBeaconCache.put(id, 0l);
+                        mBeaconCache.put(id, Calendar.getInstance());
 
                         String data = "{"
                                 + "\"SessionId\":\"" + mSessionId + "\","
@@ -68,8 +74,8 @@ public class EstimoteManager implements IAsyncTask.OnPostExecuteListener
                                 + "\"BeaconMajorVersion\":\"" + beacon.getMajor() + "\","
                                 + "\"BeaconMinorVersion\":\"" + beacon.getMinor() + "\""
                                 + "}";
-                        LogCat.log(_this, data);
-                        WebRequest.send("http://geocar.is-a-techie.com/api/registertag", data, "registertag", _this);
+
+                        WebRequest.send("http://geocar.is-a-techie.com/api/registertag", data, id, _this);
                     }
                 }
                 else
@@ -90,11 +96,25 @@ public class EstimoteManager implements IAsyncTask.OnPostExecuteListener
     {
         try
         {
-            LogCat.log(this, ((JsonDocument)result));
+            RegisterTagResult _result = new RegisterTagResult((JsonDocument)result);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MINUTE, _result.lockoutTime);
+            mBeaconCache.put(tag, calendar);
+
+            if (_result.isSuccessful())
+            {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext)
+                        .setSmallIcon(android.support.v7.appcompat.R.drawable.notification_template_icon_bg)
+                        .setContentTitle("GeoCar Points")
+                        .setContentText("You gained " + _result.pointsScored + " points! Total: " + _result.usablePoints);
+                NotificationManager manager = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(1, builder.build());
+            }
         }
         catch (Exception ex)
         {
-            LogCat.error(this, "here" + ex);
+            LogCat.error(this, ex);
         }
     }
 
