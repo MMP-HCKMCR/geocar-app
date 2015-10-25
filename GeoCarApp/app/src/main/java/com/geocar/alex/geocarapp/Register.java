f642b97a5e691c19599fb99cb6e340e4c6b7d980
Alex Bamford <Occidio001@gmail.com>
2015-10-25 07:44:08 +0000

    Update to Register and UI fixes.

--- GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/Register.java
+++ GeoCarApp/app/src/main/java/com/geocar/alex/geocarapp/Register.java
@@ -1,9 +1,12 @@
 package com.geocar.alex.geocarapp;
 
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
